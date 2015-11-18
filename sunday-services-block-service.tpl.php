de1bf8d42396b42b72dfef787955fed957868a34
Richard Franks <git@rf152.co.uk>
2015-11-18 14:07:45 +0000

    Initial Import: v1.0

---
+++ sunday-services-block-service.tpl.php
@@ -0,0 +1,8 @@
+<div class="service">
+  <div class="type">
+    <?php echo $service['type']; ?>
+  </div>
+  <div class="time-location">
+    <?php echo $service['time']; ?> at <?php echo $service['location']; ?>
+  </div>
+</div>
